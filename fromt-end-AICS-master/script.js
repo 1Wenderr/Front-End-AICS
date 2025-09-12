62b7babaf01f59a86192aeb63a75831262ebc96f
1Wenderr <wender.penha@aluno.senai.br>
2025-09-12 16:04:16 -0300

    first commit

--- fromt-end-AICS-master/script.js
+++ fromt-end-AICS-master/script.js
@@ -6,3 +6,11 @@ function mostrarSenhaNovamente() {
   const campo = document.getElementById("senhaNovamente");
   campo.type = campo.type === "password" ? "text" : "password";
 }
+
+$(document).ready(function() {
+  $('#mobile_btn').on('click', function() {
+    $('#mobile_menu').toggleClass('active');
+    $('#mobile_btn').find('i').toggleClass('fa-x');
+
+  });
+});
